cf900d3db916b65c66e0bd9ec15c5679c80ffa6a
Guillem Domènech <guillem.domenech@enti.cat>
2021-03-09 18:59:37 +0100

    Initial Commit

---
+++ AA1_MasterMind/AA1_MasterMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  AA1_MasterMindApp.swift
+//  AA1_MasterMind
+//
+//  Created by Guillem Domènech Rofín on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AA1_MasterMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
